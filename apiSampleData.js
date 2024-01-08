ced259a1efc5ff14c95991899f049a58c40a6353
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-08 20:34:01 +0530

    updated gitignore

---
+++ apiSampleData.js
@@ -0,0 +1,19 @@
+const singup = {
+    "first_name" :"ikram",
+    "last_name" : "bagban",
+    "rate_code" : "dubai",
+    "area" : "jubel ali",
+    "street_name" : "street 1",
+    "apartment" : "c2",
+    "address" : "address",
+    "contact_number" : "23233223",
+    "alter_Contact_Number" : "23233223",
+    "email" : "test@gmail.com",
+    "Password" : "123123",
+    "confirmPassword" : "123123"
+}
+
+const login = {
+    "email" : "test@gmail.com",
+    "Password" : "123123"
+}
