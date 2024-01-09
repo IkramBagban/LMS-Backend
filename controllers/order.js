3c8f6c3dc255f351f50bca566d088d683a543661
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-09 23:42:57 +0530

    removed unneccsary comments

--- controllers/order.js
+++ controllers/order.js
@@ -18,40 +18,7 @@ exports.postOrder = async (req, res, next) => {
     customerID,
   } = req.body;
 
-  // const dummyData = {
-  //     branch: "YourDefaultBranchId",
-  //     SpecialRequests: "Some special requests",
-  //     customerID: "659c176d535467ac2730c931", // Replace with the actual customer ID
-  //     custName: "YourCustomerId", // Replace with the actual customer name or ID
-  //     subtotal: "100.00", // Replace with the actual subtotal
-  //     deliveryType: "YourDeliveryType", // Replace with the actual delivery type
-  //     pickupDate: "2022-01-15", // Replace with the actual pickup date
-  //     order_source: "Mobile",
-  //     emirate_id: "YourEmirateId", // Replace with the actual Emirate ID
-  //     orderDelete: "-",
-  //     order_item: [
-  //     {
-  //         item: "YourItemId1", // Replace with the actual item ID
-  //         service: "YourServiceId1", // Replace with the actual service ID
-  //         DELIVERY: "YourDeliveryType1", // Replace with the actual delivery type
-  //         qty: "2", // Replace with the actual quantity
-  //         Price: "50.00", // Replace with the actual price
-  //     },
-  //     {
-  //         item: "YourItemId2", // Replace with the actual item ID
-  //         service: "YourServiceId2", // Replace with the actual service ID
-  //         DELIVERY: "YourDeliveryType2", // Replace with the actual delivery type
-  //         qty: "3", // Replace with the actual quantity
-  //         Price: "30.00", // Replace with the actual price
-  //     },
-  //     // Add more items as needed
-  //     ],
-  //     deliveryDate: "2022-01-20", // Replace with the actual delivery date
-  // };
-
   try {
-    // const order = new Order(dummyData)
-
     const order = new Order({
       branch,
       SpecialRequests,
@@ -71,11 +38,9 @@ exports.postOrder = async (req, res, next) => {
     const customer = await Customer.findById(customerID);
     await customer.addOrder(response._id);
     if (response) {
-      res
-        .status(201)
-        .json({
-          message: "order has been created successfully and using body data..",
-        });
+      res.status(201).json({
+        message: "order has been created successfully and using body data..",
+      });
     }
   } catch (err) {
     res.status(500).json({ message: "order failed" });
