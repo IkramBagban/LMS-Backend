ef620378d27d6c89cc4f3ee0d59cfc104b22a399
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-14 13:11:18 +0530

    fixied some issue

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,11 @@
 
 const Product = require("../models/product");
-const { data } = require("../productdata");
+// const { data } = require("../productdata");
 
 exports.getProducts = async (req,res) =>{
     // await Product.create(data)
    const products =  await Product.find()
-   console.log('data', data.length)
+//    console.log('data',products)
    
-    res.json(data);
+    res.json(products);
 }
